c9287cf3e3c34d448f6f072d719842c9dd827c7d
kimhxsong <kimhxsong@gmail.com>
2022-06-30 13:29:42 +0900

    Add cluster-setup scripts

---
+++ cluster/set_docker.sh
@@ -0,0 +1,10 @@
+brew install docker-machine docker docker-compose
+
+mkdir -p ~/goinfre/.docker
+ln -sf ~/goinfre/.docker ~/.docker
+
+docker-machine kill dev
+docker-machine rm dev
+docker-machine create --driver virtualbox dev
+
+echo ‘$(eval docker-machine env dev)’ > ~/.zprofile
---
+++ cluster/vm_get_docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#Install using the convenience script
+curl -fsSL https://get.docker.com -o get-docker.sh
+DRY_RUN=1 sh ./get-docker.sh
